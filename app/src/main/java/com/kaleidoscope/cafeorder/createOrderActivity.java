28cdd38e8db86cdad139f9e3ed8d869b27879294
GregoryError <kolesnikoff2085@yandex.ru>
2022-04-12 08:08:52 +0300

    cofe_order start form

---
+++ app/src/main/java/com/kaleidoscope/cafeorder/createOrderActivity.java
@@ -0,0 +1,14 @@
+package com.kaleidoscope.cafeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+    }
+}
